ea00f467b7cde5b03e620fd733773540656c2b92
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-21 20:34:44 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAnimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("30kg","2.0","Kitty");
+            cat.PrintInfo();
+            Console.ReadLine();
+        }
+    }
+}
